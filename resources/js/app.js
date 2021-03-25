eb98f7bc7e5d4871b19ef380cf9729fc66557e92
Alin Tentu <alin.tentu@ymail.com>
2021-03-25 18:57:18 +0200

    done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import Vue from 'vue'
+window.Vue = Vue
 require('./bootstrap');
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.component('app', require('./vue/app.vue').default);
+
+
+
+const App = new Vue({
+    el: '#app',
+    // components: { App }
+});
